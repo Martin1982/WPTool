c1eb6fa774d451a521d56eaed5bc4764d592a958
Martin de Keijzer <martin@dikketoeters.nl>
2012-05-05 18:38:54 +0200

    started working on bootstrapping

--- resources/database/setup_wptools_db.sql
+++ resources/database/setup_wptools_db.sql
@@ -1,2 +1,5 @@
+CREATE TABLE version (id INTEGER PRIMARY KEY AUTOINCREMENT, resource VARCHAR, version FLOAT);
+INSERT INTO version ('', 'database', 0.1);
+
 CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR, password VARCHAR);
 INSERT INTO users ('', 'admin', 'admin');
